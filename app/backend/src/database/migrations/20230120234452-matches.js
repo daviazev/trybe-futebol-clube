abfb1d6453f1b5fd05fbf9f937b73637a2e22936
daviazevdo <azvd.davi@gmail.com>
2023-01-20 22:57:00 -0300

    requisito 18 - migration e model matches

---
+++ app/backend/src/database/migrations/20230120234452-matches.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      home_team_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      in_progress: Sequelize.BOOLEAN
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('matches')
+  }
+};
---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,52 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+
+import db from '.';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: BOOLEAN,
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  tableName: 'matches',
+  modelName: 'Matches',
+});
