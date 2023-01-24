0c737d4ce69012d8b5377933520246fc27227866
daviazevdo <azvd.davi@gmail.com>
2023-01-24 16:42:40 -0300

    corrige erro - requisito 26 funcionando

--- app/backend/src/database/middlewares/teams.middleware.ts
+++ app/backend/src/database/middlewares/teams.middleware.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import TeamsService from '../service/teams.service';
 
-const validateIfTeamsExists = (req: Request, res: Response, next: NextFunction) => {
+const validateIfTeamsExists = async (req: Request, res: Response, next: NextFunction) => {
   const { homeTeamId, awayTeamId } = req.body;
 
   const teamsService = new TeamsService();
 
-  const result = teamsService.doesTeamsExists(homeTeamId, awayTeamId);
+  const result = await teamsService.doesTeamsExists(homeTeamId, awayTeamId);
 
   if (!result) return res.status(404).json({ message: 'There is no team with such id!' });
 
